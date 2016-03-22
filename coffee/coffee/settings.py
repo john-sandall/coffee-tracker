ad7619e95fe8521aac372fbecf3807c54a5ccb6d
John Sandall <john.sandall@gmail.com>
2016-03-22 13:36:12 +0000

    Add tracker app and register with admin site

--- coffee/coffee/settings.py
+++ coffee/coffee/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'tracker.apps.TrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- coffee/tracker/models.py
+++ coffee/tracker/models.py
@@ -1,5 +1,19 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+@python_2_unicode_compatible  # only if you need to support Python 2
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
